b6780c016459fe594aecd5bea6d1da6ef613800c
SAJEB KHAN <sajebk007@gmail.com>
2025-08-30 15:47:45 +0530

    Service layer implementated

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,17 +11,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryController {
-    private List<Category> categories=new ArrayList<>();
+
+    @Autowired
+    private  CategoryService categoryService;
+
 
     @GetMapping("/public/categories")
     public List<Category> getCategories(){
-        return categories;
+        return categoryService.getAllCategory();
         
     }
 
     @PostMapping("/public/categories")
     public String createCategory(@RequestBody Category category){
-        categories.add(category);
+        categoryService.createCategory(category);
         return "Category added successfully";
     }
 }
