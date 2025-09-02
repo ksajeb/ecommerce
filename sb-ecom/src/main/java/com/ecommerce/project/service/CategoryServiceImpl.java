7a57e8097170ea4665bba55402e0ed1d8c53c435
SAJEB KHAN <sajebk007@gmail.com>
2025-09-02 17:23:53 +0530

    api exception handling

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exception.ApiException;
 import com.ecommerce.project.exception.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepository;
@@ -28,6 +29,10 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        Category savedCategory=categoryRepository.findByCategoryName(category.getCategoryName());
+        if(savedCategory!=null){
+            throw new ApiException("Category with the name "+category.getCategoryName()+" is already exist!!!");
+        }
         category.setCategoryId(null);
         categoryRepository.save(category);
     }
